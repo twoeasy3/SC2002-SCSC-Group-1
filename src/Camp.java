9bc35eead7250b805fcca07fb800148f90854446
twoeasy3 <twoeasythree@gmail.com>
2023-10-22 16:11:44 +0800

    Incremental change to Camp class

--- src/Camp.java
+++ src/Camp.java
@@ -5,8 +5,25 @@ public class Camp {
 	String name;
 	String faculty;
 	int id; //identifier for camp 
+	int maxSize;
 	LocalDate startDate;
-	LocalDate endDate;	
+	LocalDate endDate;
+	String inCharge; //creator of camp
 	
+	String location;
+	String description;
+	//TODO implement committee and attendee slots
+	
+	
+	public Camp(String name, String faculty, int size, LocalDate start, LocalDate end, String creator) {
+		this.name = name;
+		this.faculty = faculty; //Let's use this field =ALL to indicate no faculty restrictions
+		//TODO code an index for campID
+		this.id = 0; //placeholder
+		this.maxSize = size;
+		this.startDate = start;
+		this.endDate = end;
+		this.inCharge = creator;
+	}
 
 }
